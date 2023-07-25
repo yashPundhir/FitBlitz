8d3721d44841b556ba919a5e9cafdc5998dd6f63
yashPundhir <yash.pundhir.prof@gmail.com>
2023-07-25 23:53:36 +0530

    SearchExercises component updated

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Typography, Stack, TextField } from "@mui/material";
 
 const SearchExercises = () => {
-	return <div>SearchExercises</div>;
+	return (
+		<Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+			<Typography>
+				Awesome Exercises You <br /> Should Know
+			</Typography>
+		</Stack>
+	);
 };
 
 export default SearchExercises;
