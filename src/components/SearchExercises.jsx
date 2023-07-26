a55a5ab6ca77af5ccabd71f53c9c41f31e3675c1
yashPundhir <yash.pundhir.prof@gmail.com>
2023-07-26 23:46:20 +0530

    SearchExercises component updated

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,12 +1,87 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Typography, Stack, TextField } from "@mui/material";
 
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
 const SearchExercises = () => {
+	const [search, setSearch] = useState("");
+
+	const [exercises, setExercises] = useState([]);
+
+	const [bodyParts, setBodyParts] = useState([]);
+
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			const bodyPartsData = await fetchData(
+				"https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+				exerciseOptions
+			);
+		};
+	}, []);
+
+	const handleSearch = async () => {
+		if (search) {
+			const exercisesData = await fetchData(
+				"https://exercisedb.p.rapidapi.com/exercises",
+				exerciseOptions
+			);
+
+			const searchedExercises = exercisesData.filter(
+				(exercise) =>
+					exercise.name.toLowerCase().includes(search) ||
+					exercise.target.toLowerCase().includes(search) ||
+					exercise.equipment.toLowerCase().includes(search) ||
+					exercise.bodyPart.toLowerCase().includes(search)
+			);
+
+			setSearch("");
+			setExercises(searchedExercises);
+		}
+	};
+
 	return (
 		<Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-			<Typography>
+			<Typography
+				fontWeight={700}
+				sx={{
+					fontSize: { lg: "44px", xs: "30px" },
+				}}
+				mb="50px"
+				textAlign="center"
+			>
 				Awesome Exercises You <br /> Should Know
 			</Typography>
+			<Box position="relative" mb="72px" className="box">
+				<TextField
+					sx={{
+						input: {
+							fontWeight: "700",
+							border: "none",
+						},
+						width: { lg: "800px", xs: "350px" },
+						backgroundColor: "#f5f5f5",
+					}}
+					height="76px"
+					value={search}
+					onChange={(e) => setSearch(e.target.value.toLowerCase())}
+					placeholder="Search Exercises"
+					type="text"
+				/>
+				<Button
+					className="search-btn"
+					sx={{
+						bgcolor: "#ff2625",
+						color: "#FFF",
+						textTransform: "none",
+						width: { lg: "175px", xs: "80px" },
+						fontSize: { lg: "20px", xs: "14px" },
+						height: "56px",
+					}}
+					onClick={handleSearch}
+				>
+					Search
+				</Button>
+			</Box>
 		</Stack>
 	);
 };
