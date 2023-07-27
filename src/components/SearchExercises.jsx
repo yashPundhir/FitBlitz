1e0731f2f13af121e752fc561a09c5584b7ac5df
yashPundhir <yash.pundhir.prof@gmail.com>
2023-07-27 12:14:07 +0530

    imports and props updated

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Typography, Stack, TextField } from "@mui/material";
 
+import HorizontalScrollbar from "./HorizontalScrollbar";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
 	const [search, setSearch] = useState("");
 
-	const [exercises, setExercises] = useState([]);
+	// const [exercises, setExercises] = useState([]);
 
 	const [bodyParts, setBodyParts] = useState([]);
 
@@ -16,7 +17,11 @@ const SearchExercises = () => {
 				"https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
 				exerciseOptions
 			);
+
+			setBodyParts(["all", ...bodyPartsData]);
 		};
+
+		fetchExercisesData();
 	}, []);
 
 	const handleSearch = async () => {
@@ -81,6 +86,19 @@ const SearchExercises = () => {
 				>
 					Search
 				</Button>
+			</Box>
+			<Box
+				sx={{
+					position: "relative",
+					width: "100%",
+					p: "20px",
+				}}
+			>
+				<HorizontalScrollbar
+					data={bodyParts}
+					bodyPart={bodyPart}
+					setBodyPart={setBodyPart}
+				/>
 			</Box>
 		</Stack>
 	);
