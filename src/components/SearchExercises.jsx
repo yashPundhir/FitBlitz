b40ed6e5056b0dd257d52803a7d5c479ccddfaf3
yashPundhir <yash.pundhir.prof@gmail.com>
2023-07-28 23:55:57 +0530

    HorizontalScrollbar props updated

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -98,6 +98,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
 					data={bodyParts}
 					bodyPart={bodyPart}
 					setBodyPart={setBodyPart}
+					isBodyParts
 				/>
 			</Box>
 		</Stack>
