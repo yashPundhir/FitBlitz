e91d9e5a33d993a9b59f73d748124b46033e157e
yashPundhir <yash.pundhir.prof@gmail.com>
2023-07-28 21:19:32 +0530

    footer updated with required content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,24 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+import Logo from "../assets/images/Logo-1.png";
+
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<Box mt="80px" bgcolor="#fff3f4">
+			<Stack gap="40px" px="40px" pt="50px" alignItems="center">
+				<img src={Logo} alt="logo" width="200px" height="40px" />
+			</Stack>
+			<Typography
+				variant="h5"
+				sx={{ fontSize: { lg: "28px", xs: "20px" } }}
+				mt="31px"
+				textAlign="center"
+				pb="50px"
+			>
+				Made with ❤️ by Yash Pundhir
+			</Typography>
+		</Box>
+	);
 };
 
 export default Footer;
